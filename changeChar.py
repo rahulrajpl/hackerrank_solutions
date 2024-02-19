d8ed6e229a0f7d9642d32785bff86c498e482831
Random Walker <lukrahul@gmail.com>
2024-02-19 21:57:08 +0530

    sol 19 Feb 24

---
+++ changeChar.py
@@ -0,0 +1,8 @@
+def mutate_string(string, position, character):
+    return string[:position]+ character + string[position+1:]
+
+if __name__ == '__main__':
+    s = 'abracadabra'
+    i, c = 5, 'k'
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
